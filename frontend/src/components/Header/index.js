c71c2d287bc04774b6bd49727ffe2087f65def97
Chriszao <chris.f.assis18@gmail.com>
2022-03-06 17:37:46 -0300

    refactor: Move InputSearch to Home component

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 
 import logo from '../../assets/images/logo.svg';
 
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 export default function Header() {
   return (
     <Container>
       <img src={logo} alt="MyContacts logo" width="201" />
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar pelo nome" />
-      </InputSearchContainer>
     </Container>
   );
 }
