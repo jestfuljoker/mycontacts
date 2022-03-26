4a58a23011f8b28a7dcf31798f999fb3e030e08b
Chriszao <chris.f.assis18@gmail.com>
2022-03-26 13:47:00 -0300

    feat: Add cors middlware

--- api/src/index.js
+++ api/src/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors);
+
 app.use(routes);
 
-app.use((error, request, response, next) => {
-  console.log(error);
+app.use(errorHandler);
 
-  response.sendStatus(500);
-});
-
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('🔥 Server started at http://localhost:3001'));
