afd93d837d3e638b55a4fafaad3d948e9b12eb63
Chriszao <chris.f.assis18@gmail.com>
2022-06-18 16:20:38 -0300

    fix: Fix env vars undefined error

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,11 +1,11 @@
-const { Client } = require("pg");
+const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   port: 5432,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 client.connect();
