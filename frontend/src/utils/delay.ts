2dde131e5d8e3d701fded5d5a7d938eeda9d8f31
Christofer Assis <chris.f.assis18@gmail.com>
2023-10-25 05:44:33 -0300

    chore(frontend): run pretty command

--- frontend/src/utils/delay.ts
+++ frontend/src/utils/delay.ts
@@ -1,5 +1,5 @@
 export default function delay(ms = 1000) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
 }
