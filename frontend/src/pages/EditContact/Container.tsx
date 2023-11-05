4bf85b3756db0e177945bff01284b479779ac271
Christofer Assis <chris.f.assis18@gmail.com>
2023-11-05 15:24:46 -0300

    refactor(frontend): Implementing Container/Presentation pattern

---
+++ frontend/src/pages/EditContact/Container.tsx
@@ -0,0 +1,71 @@
+import type { ContactFormRef } from '@components/ContactForm';
+import { useSafeAsyncAction } from '@hooks/useSafeAsyncAction';
+import ContactsService from '@services/ContactsService';
+import type { DomainContactData } from '@services/mappers/ContactMapper';
+import { toast } from '@utils/toast';
+import { useEffect, useRef, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import Presentation from './Presentation';
+
+export default function Container() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [contactName, setContactName] = useState('');
+
+	const { id } = useParams<{ id: string }>();
+	const history = useHistory();
+	const contactFormRef = useRef<ContactFormRef | null>(null);
+
+	const safeAsyncAction = useSafeAsyncAction();
+
+	useEffect(() => {
+		async function loadContact() {
+			try {
+				const contact = await ContactsService.getContactById(id);
+
+				safeAsyncAction(() => {
+					contactFormRef.current?.setFieldsValues(contact);
+
+					setIsLoading(false);
+					setContactName(contact.name);
+				});
+			} catch {
+				safeAsyncAction(() => {
+					toast({
+						text: 'Contato não encontrado!',
+						type: 'danger',
+					});
+					history.push('/');
+				});
+			}
+		}
+
+		loadContact();
+	}, [history, id, safeAsyncAction]);
+
+	async function handleSubmit(contact: DomainContactData) {
+		try {
+			const updatedContact = await ContactsService.updateContact(id, contact);
+
+			setContactName(updatedContact.name);
+
+			toast({
+				text: `Contato "${contact.name}" atualizado com sucesso!`,
+				type: 'success',
+			});
+		} catch {
+			toast({
+				text: `Ocorreu um error ao atualizar o contato "${contact.name}"!`,
+				type: 'danger',
+			});
+		}
+	}
+
+	return (
+		<Presentation
+			contactFormRef={contactFormRef}
+			contactName={contactName}
+			isLoading={isLoading}
+			onSubmit={handleSubmit}
+		/>
+	);
+}
